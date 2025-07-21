c81ea72433b80139c039e5db4972fb9efa96f6b0
condua <phuc.phanphuc2002@hcmut.edu.vn>
2025-07-21 21:39:18 +0700

    Thêm các routes của admin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,10 +5,6 @@ const {
   updateUserById,
   enrollCourse,
   getCurrentUser,
-  // ✨ Hàm mới cho admin
-  createUserByAdmin,
-  updateUserByAdmin,
-  deleteUserByAdmin,
 } = require("../controllers/userController");
 const {
   authMiddleware,
@@ -18,24 +14,7 @@ const {
 const router = express.Router();
 
 // router.get("/:id", authMiddleware, getUserById);
-/*-----------------------------------*\
-|           ROUTES FOR ADMIN          |
-\*-----------------------------------*/
-// Lấy tất cả người dùng
 router.get("/", authMiddleware, adminMiddleware, getAllUsers);
-
-// ✨ TẠO người dùng mới
-router.post("/", authMiddleware, adminMiddleware, createUserByAdmin);
-
-// Lấy người dùng bất kỳ theo ID
-router.get("/:id", authMiddleware, adminMiddleware, getUserById);
-
-// ✨ CẬP NHẬT người dùng bất kỳ theo ID
-router.put("/:id", authMiddleware, adminMiddleware, updateUserByAdmin);
-
-// ✨ XÓA người dùng bất kỳ theo ID
-router.delete("/:id", authMiddleware, adminMiddleware, deleteUserByAdmin);
-
 router.get("/me", authMiddleware, getCurrentUser);
 router.put("/:id", authMiddleware, updateUserById);
 router.post("/enroll", authMiddleware, enrollCourse);
