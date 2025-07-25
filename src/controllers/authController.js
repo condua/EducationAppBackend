2083fd106888bc7b136ad549f23a889b6a1ac03f
condua <phuc.phanphuc2002@hcmut.edu.vn>
2025-07-25 14:03:46 +0700

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ cloudinary.config({
 // Tạo JWT Token
 const generateToken = (user) => {
   return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: "10y",
+    expiresIn: 60 * 60 * 24 * 365 * 10,
   });
 };
 
